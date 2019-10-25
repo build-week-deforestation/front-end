92be71621082bb7c4d083ff810fbaf2cef319699
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-10-24 21:46:53 -0500

    Working on state management.

--- deforestation/src/App.jsx
+++ deforestation/src/App.jsx
@@ -1,79 +1,70 @@
-import React, { createContext } from 'react';
+import React, { useReducer } from "react";
+import { AuthContext } from "./Components/Context";
 import { Route, Link } from "react-router-dom";
 import styled from "styled-components";
-import Registration from './Components/registration';
+import Registration from "./Components/registration";
 import UserPage from "./Components/UserPage";
 import Dashboard from "./Components/Dashboard";
-
-
+import { BrowserRouter as Router } from "react-router-dom";
+// import Routes from "./Components/Routes"
 
 // //body color: #e8ecf1,
 //  Primary color:  #03bd37,
 //   Accent 1: #f0f8ff,
-//   Accent 2: #3d7c47, 
+//   Accent 2: #3d7c47,
 //   Accent 3: #2c82c9
-
 
 //Setting up state for authentication
 
-const AuthContext = createContext();
-
-const state = {
-  isAuthenticated: false,
-  user: null,
-  token: null
-};
-
-//Reducer pattern for rendering based on whether the user is logged in using a switch method.
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN":
-      localStorage.setItem("user", JSON.stringify(action.payload.user));
-      localStorage.setItem("token", JSON.stringify(action.payload.token));
-      return {
-        ...state,
-        isAuthenticated: true,
-        user: action.payload.user,
-        token: action.payload.token
-      };
-    case "LOGOUT":
-      localStorage.clear();
-      return {
-        ...state,
-        isAuthenticated: false,
-        user: null
-      };
-    default:
-      return state;
-  }
+const initialState = {
+  isAuthenticated: false
+  // user: null,
+  // token: null
 };
 
 const Wrapper = styled.div`
-
-font-family: 'Roboto Mono', monospace;
-background-color: #22313f;
-
+  font-family: "Roboto Mono", monospace;
+  background-color: #22313f;
 `;
 
-
-
 function App() {
+  const [state, dispatch] = useReducer((initialState, action) => {
+    switch (action.type) {
+      case "LOGIN":
+        return {
+          ...state,
+          isAuthenticated: true
+        };
+      case "LOGOUT":
+        localStorage.clear();
+        return {
+          ...state,
+          isAuthenticated: false
+        };
+      default:
+        return state;
+    }
+  });
   return (
+    <Router>
       <Wrapper>
         <AuthContext.Provider
           value={{
-            state
-
+            state,
+            dispatch
           }}
         >
-          {!state.isAuthenticated ?   <Route path="/" component={Registration}></Route> : <Route path="/" component={Dashboard}></Route>}
+          {!initialState.isAuthenticated ? (
+            <Route path="/" component={Registration}></Route>
+          ) : (
+            <Route path="/" component={Dashboard}></Route>
+          )}
 
-        <Route path="/user-page" component={UserPage}></Route>
-        <Route path="/dashboard" component={Dashboard}></Route>
-
+          <Route path="/user-page" component={UserPage}></Route>
+          <Route path="/dashboard" component={Dashboard}></Route>
         </AuthContext.Provider>
       </Wrapper>
-    
+    </Router>
   );
 }
 
--- deforestation/src/Components/registration.jsx
+++ deforestation/src/Components/registration.jsx
@@ -1,49 +1,42 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
-import { RegistrationStylesMain, RegistrationStylesCard } from "../Styles/Registration";
+import {
+  RegistrationStylesMain,
+  RegistrationStylesCard
+} from "../Styles/Registration";
 
 import RegistrationForm from "./RegistrationForm";
-
 
 // //body color: #e8ecf1,
 //  Primary color:  #03bd37,
 //   Accent 1: #f0f8ff,
-//   Accent 2: #3d7c47, 
+//   Accent 2: #3d7c47,
 //   Accent 3: #2c82c9
 
+const RegistrationContainer = styled.div`
+  ${RegistrationStylesMain};
+`;
 
-const RegistrationContainer = styled.div `${RegistrationStylesMain};`;
+const RegistrationCard = styled.section`
+  ${RegistrationStylesCard};
+`;
 
-const RegistrationCard = styled.section`${RegistrationStylesCard};`;
+function Registration(props) {
+  return (
+    <RegistrationContainer className="RegCont">
+      <div className="main-content-div">
+        <div className="breakpoint-div"></div>
+        <RegistrationCard>
+          <h2>Discover More</h2>
 
-
-function Registration (props) {
-
-  
-    return(
-        
-        <RegistrationContainer className="RegCont">
-
-
-
-            <div className="main-content-div">
-
-                <div className="breakpoint-div">
-
-                </div>
-                <RegistrationCard>
-
-                    <h2>Discover More</h2>
-                
-                    <RegistrationForm arr={["albania", "romania", "lithuania", "United States"]}/>
-                
-                </RegistrationCard>
-            </div>
-            
-        </RegistrationContainer>           
-    );
+          <RegistrationForm
+            arr={["albania", "romania", "lithuania", "United States"]}
+          />
+        </RegistrationCard>
+      </div>
+    </RegistrationContainer>
+  );
 }
 
-
-export default Registration; 
+export default Registration;
